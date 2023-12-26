bd65572c85220a8c7efd6ba04e29a514fd3abf3e
tepavcevic <djordje.tepa@gmail.com>
2023-12-26 15:13:56 +0100

    Change notes side menu layout

--- app/routes/users+/$userId_+/notes.tsx
+++ app/routes/users+/$userId_+/notes.tsx
@@ -3,25 +3,27 @@ import { Link, NavLink, Outlet } from "@remix-run/react";
 export default function NotesRoute() {
   return (
     <div className="flex h-full justify-between pb-12 border-8 border-blue-500">
-      <h1 className="text-h1">Notes</h1>
+      <div>
+        <h1 className="text-h1">Notes</h1>
 
-      <ul>
-        <li>
-          <Link to=".." relative="path">
-            Back to user
-          </Link>
-        </li>
-        <li>
-          <NavLink
-            to="some-note-id"
-            className={({ isActive }) =>
-              `underline ${isActive ? "bg-accent" : ""}`
-            }
-          >
-            Some note
-          </NavLink>
-        </li>
-      </ul>
+        <ul>
+          <li>
+            <Link to=".." relative="path">
+              Back to user
+            </Link>
+          </li>
+          <li>
+            <NavLink
+              to="some-note-id"
+              className={({ isActive }) =>
+                `underline ${isActive ? "bg-accent" : ""}`
+              }
+            >
+              Some note
+            </NavLink>
+          </li>
+        </ul>
+      </div>
       <Outlet />
     </div>
   );
