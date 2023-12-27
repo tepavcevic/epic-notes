ff6e25c438433dc8b1b29af3a4574f479631657a
tepavcevic <djordje.tepa@gmail.com>
2023-12-27 11:33:14 +0100

    Use loaders for data loading

--- app/routes/users+/$username.tsx
+++ app/routes/users+/$username.tsx
@@ -1,11 +1,23 @@
-import { Link, useParams } from "@remix-run/react";
+import { Link, useLoaderData } from "@remix-run/react";
+import { json } from "@remix-run/node";
+
+export function loader() {
+  return json({
+    id: "9d6eba59daa2fc2078cf8205cd451041",
+    email: "kody@kcd.dev",
+    username: "kody",
+    name: "Kody",
+  });
+}
 
 export default function UserProfileRoute() {
-  const { username } = useParams();
+  const loaderData = useLoaderData<typeof loader>();
   return (
-    <div className="container mb-48 mt-36 border-4 border-green-500">
-      <h1 className="text-h1">{username}</h1>
-      <Link to="notes">Notes</Link>
+    <div className="container mb-48 mt-36">
+      <h1 className="text-h1">{loaderData.username || "User"}</h1>
+      <Link to="notes" className="underline">
+        Notes
+      </Link>
     </div>
   );
 }
