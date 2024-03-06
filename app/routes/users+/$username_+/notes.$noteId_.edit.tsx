497abfb06c050899ceb5c89c77c5acaaa6df469e
tepavcevic <djordje.tepa@gmail.com>
2024-03-06 20:29:30 +0100

    Adding some basic form validation

--- app/routes/users+/$username_+/notes.$noteId_.edit.tsx
+++ app/routes/users+/$username_+/notes.$noteId_.edit.tsx
@@ -1,6 +1,7 @@
 import { json, redirect, type LoaderFunctionArgs } from '@remix-run/node'
 import {
 	Form,
+	useActionData,
 	useFormAction,
 	useLoaderData,
 	useNavigation,
@@ -14,6 +15,9 @@ import { Button } from '#app/components/ui/button.tsx'
 import { floatingToolbarClassName } from '#app/components/floating-toolbar.tsx'
 import { StatusButton } from '#app/components/ui/status-button.tsx'
 import { GeneralErrorBoundary } from '#app/components/error-boundary.tsx'
+
+const MAX_TITLE_LENGTH = 100
+const MAX_CONTENT_LENGTH = 10000
 
 export async function loader({ params }: LoaderFunctionArgs) {
 	const note = db.note.findFirst({
@@ -36,6 +40,40 @@ export async function action({ request, params }: LoaderFunctionArgs) {
 	const title = formData.get('title')
 	const content = formData.get('content')
 
+	const errors = {
+		formErrors: [] as Array<string>,
+		fieldErrors: {
+			title: [] as Array<string>,
+			content: [] as Array<string>,
+		},
+	}
+
+	if (title === '') {
+		errors.fieldErrors.title.push('Title is required')
+	}
+	if (title && title.toString()?.length > MAX_TITLE_LENGTH) {
+		errors.fieldErrors.title.push(
+			`Title must be less than ${MAX_TITLE_LENGTH} characters`,
+		)
+	}
+	if (content === '') {
+		errors.fieldErrors.content.push('Content is required')
+	}
+	if (content && content.toString()?.length > MAX_CONTENT_LENGTH) {
+		errors.fieldErrors.content.push(
+			`Content must be less than ${MAX_CONTENT_LENGTH} characters`,
+		)
+	}
+
+	const hasErrors =
+		Object.values(errors.fieldErrors).some(
+			fieldErrors => fieldErrors.length > 0,
+		) || errors.formErrors.length > 0
+
+	if (hasErrors) {
+		return json({ status: 'error', errors } as const, { status: 400 })
+	}
+
 	invariantResponse(typeof title === 'string', 'Title must be a string')
 	invariantResponse(typeof content === 'string', 'Content must be a string')
 
@@ -54,10 +92,29 @@ export async function action({ request, params }: LoaderFunctionArgs) {
 	return redirect(`/users/${params.username}/notes/${params.noteId}`)
 }
 
+function ErrorList({ errors }: { errors?: Array<string> | null }) {
+	return errors?.length ? (
+		<ul className="flex flex-col gap-1">
+			{errors.map(error => (
+				<li key={error} className="text-[10px] text-destructive">
+					{error}
+				</li>
+			))}
+		</ul>
+	) : null
+}
+
 export default function NoteEdit() {
 	const data = useLoaderData<typeof loader>()
+	const actionData = useActionData<typeof action>()
 	const navigation = useNavigation()
 	const formAction = useFormAction()
+	const formId = 'form-editor'
+
+	const fieldErrors =
+		actionData?.status === 'error' ? actionData?.errors?.fieldErrors : null
+	const formErrors =
+		actionData?.status === 'error' ? actionData?.errors?.formErrors : null
 
 	const isPending =
 		navigation.state !== 'idle' &&
@@ -65,38 +122,62 @@ export default function NoteEdit() {
 		navigation.formMethod === 'POST'
 
 	return (
-		<Form method="POST" className="flex flex-col gap-4 p-12">
-			<div>
-				<Label htmlFor="title">Title</Label>
-				<Input
-					id="title"
-					name="title"
-					defaultValue={data.note.title}
-					disabled={isPending}
-				/>
-			</div>
-			<div>
-				<Label htmlFor="content">Content</Label>
-				<Textarea
-					id="content"
-					name="content"
-					defaultValue={data.note.content}
-					disabled={isPending}
-				/>
-			</div>
-			<div className={floatingToolbarClassName}>
-				<Button type="reset" className="mr-2 bg-red-700" disabled={isPending}>
-					Reset Form
-				</Button>
-				<StatusButton
-					status={isPending ? 'pending' : 'idle'}
-					type="submit"
-					disabled={isPending}
-				>
-					Submit
-				</StatusButton>
-			</div>
-		</Form>
+		<div className="absolute inset-0">
+			<Form
+				id={formId}
+				method="POST"
+				className="flex flex-col gap-4 p-12"
+				noValidate
+			>
+				<div>
+					<Label htmlFor="title">Title</Label>
+					<Input
+						id="title"
+						name="title"
+						defaultValue={data.note.title}
+						disabled={isPending}
+						required
+						maxLength={MAX_TITLE_LENGTH}
+						autoComplete="off"
+						autoCorrect="off"
+					/>
+					<div className="min-h-[32px] px-4 pt-1 pb-3">
+						<ErrorList errors={fieldErrors?.title} />
+					</div>
+				</div>
+				<div>
+					<Label htmlFor="content">Content</Label>
+					<Textarea
+						id="content"
+						name="content"
+						defaultValue={data.note.content}
+						disabled={isPending}
+						required
+						maxLength={MAX_CONTENT_LENGTH}
+						autoComplete="off"
+						autoCorrect="off"
+					/>
+					<div className="min-h-[32px] px-4 pt-1 pb-3">
+						<ErrorList errors={fieldErrors?.content} />
+					</div>
+				</div>
+				<div className={floatingToolbarClassName}>
+					<Button type="reset" className="mr-2 bg-red-700" disabled={isPending}>
+						Reset Form
+					</Button>
+					<StatusButton
+						status={isPending ? 'pending' : 'idle'}
+						type="submit"
+						disabled={isPending}
+					>
+						Submit
+					</StatusButton>
+				</div>
+				<div className="min-h-[32px] px-4 pt-1 pb-3">
+					<ErrorList errors={formErrors} />
+				</div>
+			</Form>
+		</div>
 	)
 }
 
