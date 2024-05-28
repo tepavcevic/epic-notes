40506e1c4c86e0b39683af5f7cd57f8755135417
tepavcevic <djordje.tepa@gmail.com>
2024-05-28 13:32:29 +0200

    Add fixture for inserting and deleting a user when testing search

--- tests/e2e/search.test.ts
+++ tests/e2e/search.test.ts
@@ -1,19 +1,54 @@
-import { expect, test } from '@playwright/test'
+import { test as base } from '@playwright/test'
+import { prisma } from '#app/utils/db.server.ts'
+import { createUser } from '#tests/db-utils.ts'
 
 const nonexistentUser = '__some_random_gibberish__'
+const test = base.extend<{
+	insertNewUser: () => Promise<{
+		id: string
+		name: string | null
+		username: string
+	}>
+}>({
+	insertNewUser: async ({}, use) => {
+		let userId: string | undefined = undefined
 
-test('Search from homepage', async ({ page }) => {
+		await use(async () => {
+			const newUser = await prisma.user.create({
+				select: { id: true, name: true, username: true },
+				data: createUser(),
+			})
+			userId = newUser.id
+			return newUser
+		})
+
+		if (userId) {
+			await prisma.user.delete({
+				where: { id: userId },
+			})
+		}
+	},
+})
+const { expect } = test
+
+test('Search from homepage', async ({ page, insertNewUser }) => {
+	const newUser = await insertNewUser()
+
 	await page.goto('/')
-	await page.getByRole('searchbox', { name: /search/i }).fill('kody')
+	await page.getByRole('searchbox', { name: /search/i }).fill(newUser.username)
 	await page.getByRole('button', { name: /search/i }).click()
 
-	await page.waitForURL('/users?search=kody')
+	await page.waitForURL(
+		`/users?${new URLSearchParams({ search: newUser.username })}`,
+	)
 	await expect(
 		page.getByRole('heading', { name: /epic notes users/i }),
 	).toBeVisible()
 	const usersList = page.getByRole('main').getByRole('list')
 	expect(usersList.getByRole('listitem')).toHaveCount(1)
-	await expect(usersList.getByAltText('kody')).toBeVisible()
+	await expect(
+		usersList.getByAltText(newUser.name ?? newUser.username),
+	).toBeVisible()
 
 	await page.getByRole('searchbox', { name: /search/i }).fill(nonexistentUser)
 	await page.getByRole('button', { name: /search/i }).click()
