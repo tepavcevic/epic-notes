629e716347b5571b43d3b18ab774a61706e1df18
tepavcevic <djordje.tepa@gmail.com>
2024-06-20 23:49:57 +0200

    Testing hook behavior using test component

--- app/utils/misc.use-double-check.test.tsx
+++ app/utils/misc.use-double-check.test.tsx
@@ -1,7 +1,9 @@
 /**
  * @vitest-environment jsdom
  */
-import { act, renderHook } from '@testing-library/react'
+import { act, render, renderHook, screen } from '@testing-library/react'
+import { userEvent } from '@testing-library/user-event'
+import { useState } from 'react'
 import { expect, test, vi } from 'vitest'
 import { useDoubleCheck } from './misc.tsx'
 
@@ -39,4 +41,45 @@ test('hook: prevents default on the first click, and does not on the second', as
 	expect(mockClick).toHaveBeenCalledTimes(1)
 	expect(mockClick).toHaveBeenCalledWith(clickEvent2)
 	expect(clickEvent2.defaultPrevented).toBe(false)
+})
+
+function TestComponent() {
+	const [defaultPrevented, setDefaultPrevented] = useState<
+		'idle' | 'no' | 'yes'
+	>('idle')
+	const dc = useDoubleCheck()
+
+	return (
+		<div>
+			<output>Default prevented: {defaultPrevented}</output>
+			<button
+				{...dc.getButtonProps({
+					onClick: event =>
+						setDefaultPrevented(event.defaultPrevented ? 'yes' : 'no'),
+				})}
+			>
+				{dc.doubleCheck ? 'Are you sure?' : 'Click me'}
+			</button>
+		</div>
+	)
+}
+
+test('Test component: prevents default on first click and does not on the second', async () => {
+	const user = userEvent.setup()
+	await render(<TestComponent />)
+
+	const status = screen.getByRole('status')
+	const button = screen.getByRole('button')
+
+	expect(status).toHaveTextContent('Default prevented: idle')
+	expect(button).toHaveTextContent('Click me')
+
+	await user.click(button)
+
+	expect(status).toHaveTextContent('Default prevented: yes')
+	expect(button).toHaveTextContent('Are you sure?')
+
+	await user.click(button)
+
+	expect(status).toHaveTextContent('Default prevented: no')
 })
